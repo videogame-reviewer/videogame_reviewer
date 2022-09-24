11e752064c1e78cd0b2ebe7f90c18d05747cea87
Luis Jimenez <luisjimenez@Luiss-Air.attlocal.net>
2022-09-23 21:00:08 -0500

    Only the comments appear on the Datebase

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Game, Review } = require('../models');
+const withAuth = require('../utils/auth')
 
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
@@ -56,7 +57,7 @@ router.get('/game/:id', async (req, res) => {
 );
 
 // GET one painting
-router.post('/game/:id', async (req, res) => {
+router.post('/game/:id',async (req, res) => {
   // If the user is not logged in, redirect the user to the login page
   // if (!req.session.loggedIn) {
   //   res.redirect('/login');
@@ -65,10 +66,10 @@ router.post('/game/:id', async (req, res) => {
     try {
     const newReview = await Review.create({
       ...req.body,
-      game_id: req.params.id,
-      user_id: req.session.user_id
+      // game_id: req.params.id,
+      user_id: req.session.user_id,
     });
-
+    console.log(newReview);
     res.status(200).json(newReview);
     } catch (err) {
       console.log(err);
